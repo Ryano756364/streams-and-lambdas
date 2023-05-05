5017e865c1bac59e7a776e0d78e63f05abd7d645
Ryan <ryanodelljavadeveloper@gmail.com>
2023-05-05 08:39:34 -0400

    setup of problem #2

--- src/main/java/com/pluralsight/streamslambdas/examples/LambdasExample02.java
+++ src/main/java/com/pluralsight/streamslambdas/examples/LambdasExample02.java
@@ -14,19 +14,9 @@ import java.util.List;
 public class LambdasExample02 {
 
     // Print the products that cost less than the price limit.
-    static void printProducts(List<Product> products, BigDecimal priceLimit) {
-        for (Product product : products) {
-            if (product.getPrice().compareTo(priceLimit) < 0) {
-                System.out.println(product);
-            }
-        }
-    }
-
     public static void main(String[] args) {
         List<Product> products = ExampleData.getProducts();
 
         BigDecimal priceLimit = new BigDecimal("5.00");
-
-        printProducts(products, priceLimit);
     }
 }
