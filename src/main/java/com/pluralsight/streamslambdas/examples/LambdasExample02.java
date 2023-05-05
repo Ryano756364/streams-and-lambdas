64d9f4d95feacd23706d6782a2706672b50c9088
Ryan <ryanodelljavadeveloper@gmail.com>
2023-05-05 08:40:58 -0400

    Possible solution that could be done -> loop with limit

--- src/main/java/com/pluralsight/streamslambdas/examples/LambdasExample02.java
+++ src/main/java/com/pluralsight/streamslambdas/examples/LambdasExample02.java
@@ -14,9 +14,20 @@ import java.util.List;
 public class LambdasExample02 {
 
     // Print the products that cost less than the price limit.
+    static void printProducts(List<Product> products, BigDecimal priceLimit) {
+        //We could run a loop to get this done.
+        for (Product product : products) {
+            if (product.getPrice().compareTo(priceLimit) < 0) {
+                System.out.println(product);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         List<Product> products = ExampleData.getProducts();
 
         BigDecimal priceLimit = new BigDecimal("5.00");
+
+        printProducts(products, priceLimit);
     }
 }
